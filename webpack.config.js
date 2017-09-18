8340ce720273e5aef1335e066ef28c670a8c4312
wenyin <342306219@qq.com>
2017-09-18 23:11:44 +0800

    区分了环境

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,17 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //生成html模板
 const ExtractTextPlugin = require('extract-text-webpack-plugin'); //提出css
 const glob = require('glob'); //循环所有文件
+const env = process.env.NODE_ENV //当前环境
+
+console.log(env);
 
 //__dirname目录地址 __filename文件地址
 
 const config = {
-    devtool: 'source-map',
-    entry: {
-        vendor: ['vue', 'vue-router', 'vuex'] // 公用方法
-    },
+    entry: {},
     output: {
         path: `${__dirname}/dist`,
         filename: '[name]-[hash].js',
-    },
-    devServer: {
-        //contentBase: `${__dirname}/dist`, // 不怎么清楚
-        historyApiFallback: true,//不跳转
-        inline: true,//实时刷新
-        hot: true, //热加载
-        port: 8080, //端口
     },
     module: {
         rules: [
@@ -39,12 +32,12 @@ const config = {
             },
             {
                 test: /\.(css|scss)$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader?sourceMap',
-                    //如果需要，可以在 sass-loader 之前将 resolve-url-loader 链接进来
-                    //use: ['css-loader?modules=true', 'postcss-loader']
-                    use: ['css-loader?sourceMap', 'postcss-loader?sourceMap']
-                })
+                use: env === 'production'
+                    ? ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'postcss-loader']
+                    })
+                    : ['style-loader?sourceMap', 'css-loader?sourceMap', 'postcss-loader?sourceMap']
             },
             {
                 test: /\.(png|jpg|gif)$/,
@@ -67,8 +60,38 @@ const config = {
             'vue$': 'vue/dist/vue.esm.js', //webpack的vue必须要加这个
         }
     },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(), //热加载插件
+    plugins: []
+}
+
+
+function getEntry(globPath, pathDir) {
+    var files = glob.sync(globPath);
+    var entries = {},
+        entry, dirname, basename, extname, chunks;
+        
+    
+    files.forEach((entry) => {
+        dirname = path.dirname(entry);
+        basename = /apps\/(.*)\/index\.js/.exec(entry)[1];
+        chunks = env === 'production' ? ['vendor', basename] : [basename];
+        entries[basename] = entry;
+        const plug =  new HtmlWebpackPlugin({
+            filename: `${__dirname}/dist/${basename}.html`,
+            chunks,
+            template: `${dirname}/index.html`,
+            inject: true
+        });
+        config.plugins.push(plug);
+    });
+    return entries;
+}
+const newEntries = getEntry('./src/apps/*/index.js');
+
+if(env === 'production') { // 生产环境
+    config.entry = {
+        vendor: ['vue', 'vue-router', 'vuex'] // 公用方法
+    };
+    config.plugins = config.plugins.concat([
         new webpack.optimize.CommonsChunkPlugin({
             names: ['vendor']
         }),
@@ -78,30 +101,20 @@ const config = {
                 return getPath('[name].[hash].css');
             },
         }) //分离CSS
-    ]
+    ]);
+} else { //开发环境
+    config.devtool = 'source-map';
+    config.devServer = {
+        //contentBase: `${__dirname}/dist`, // 不怎么清楚
+        historyApiFallback: true,//不跳转
+        inline: true,//实时刷新
+        hot: true, //热加载
+        port: 8080, //端口
+    };
+    config.plugins = config.plugins.concat([
+        new webpack.HotModuleReplacementPlugin(), //热加载插件
+    ]);
 }
-
-
-function getEntry(globPath, pathDir) {
-    var files = glob.sync(globPath);
-    var entries = {},
-        entry, dirname, basename, extname;
-    
-    files.forEach((entry) => {
-        dirname = path.dirname(entry);
-        basename = /apps\/(.*)\/index\.js/.exec(entry)[1];
-        entries[basename] = entry;
-        const plug =  new HtmlWebpackPlugin({
-            filename: `${__dirname}/dist/${basename}.html`,
-            chunks: ['vendor', basename],
-            template: `${dirname}/index.html`,
-            inject: true
-        });
-        config.plugins.push(plug);
-    });
-    return entries;
-}
-const newEntries = getEntry('./src/apps/*/index.js');
 
 config.entry = Object.assign({}, config.entry, newEntries);
 
